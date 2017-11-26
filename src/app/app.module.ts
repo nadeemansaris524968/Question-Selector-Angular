e189e55727756490157e0c2b4c9b45261911a167
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-11-26 13:42:53 -0800

    Add home, all-questions, selected question and navbar components.
    Configured routes to each.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AllQuestionsComponent } from './all-questions/all-questions.component';
+import { SelectedQuestionComponent } from './selected-question/selected-question.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AllQuestionsComponent,
+    SelectedQuestionComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'all-questions', component: AllQuestionsComponent },
+      { path: 'all-questions/:id', component: SelectedQuestionComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
