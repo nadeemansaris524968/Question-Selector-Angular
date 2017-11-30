d83aa7e3083825d907148b8638fa511d762c9041
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-11-30 13:40:04 -0800

    Now creating QA Format objects for each independent, if and then questions.

--- src/app/selected-question/selected-question.component.ts
+++ src/app/selected-question/selected-question.component.ts
@@ -17,7 +17,6 @@ import { Question } from '../models/question';
 export class SelectedQuestionComponent implements OnInit {
   id: string;
   cumulativeQuestion: any;
-  independentQuestionText: string;
   independentQuestions: any[];
   ifThens: any[];
   img: string;
@@ -47,51 +46,12 @@ export class SelectedQuestionComponent implements OnInit {
   independentAnswer(independentQuestion: any, independentRadio: HTMLInputElement) {
 
     independentQuestion["answerChoice"] = independentRadio.value;
-
-    let question = this.getQuestion(
-                      independentQuestion.text, 
-                      independentQuestion.answers, 
-                      independentRadio.value);
-
-    // construct RootCase - set Question
-    let rootCase = new RootCase();
-    rootCase.addQuestions(question);
-
-    console.log(JSON.stringify(rootCase, undefined, 2));
-  }
-
-  getImageURL() {
-    return this.cumulativeQuestion.img;
-  }
-
-  getQuestionMedia() {
-    let media = new Media();
-
-    let questionMedia = new QuestionMedia();
-    questionMedia.setMediaURL(this.getImageURL());
-    questionMedia.setMedia(media);
-
-    return questionMedia;
-  }
-
-  getQuestionText(text: string) {
-    let questionText = new QuestionText();
-    questionText.addText(text);
-
-    return questionText;
-  }
-
-  getQuestion(text: string, answerChoices: string[], correctAnswerChoice: string) {
-    let question = new Question();
-    question.setQuestionText(this.getQuestionText(text));
-    question.addQuestionMedia(this.getQuestionMedia());
-    for (var i = 0; i < answerChoices.length; i++) {
-      question.addAnswerChoices(new AnswerChoice(answerChoices[i]));
-    }
-
-    question.addCorrectAnswerChoice(correctAnswerChoice);
-
-    return question;
+    let rootCase = this.getQuestionAnswering(
+      independentQuestion.text,
+      independentQuestion.answers,
+      independentRadio.value
+    );
+    // console.log("INDEPENDENT Answer *************\n" + JSON.stringify(rootCase, undefined, 2));
   }
 
   ifAnswer(ifQuestion: any, ifRadio: HTMLInputElement) {
@@ -102,15 +62,74 @@ export class SelectedQuestionComponent implements OnInit {
       ifQuestion.if_question["showThenQuestions"] = false;
 
     ifQuestion.if_question["answerChoice"] = ifRadio.value;
+
+    let rootCase = this.getQuestionAnswering(
+      ifQuestion.if_question.text,
+      ifQuestion.if_question.answers,
+      ifRadio.value
+    );
+    // console.log("If Answer *************\n" + JSON.stringify(rootCase, undefined, 2));
   }
 
   thenAnswer(thenQuestion: any, thenRadio: HTMLInputElement) {
 
     thenQuestion["answerChoice"] = thenRadio.value;
+
+    let rootCase = this.getQuestionAnswering(
+      thenQuestion.text,
+      thenQuestion.answers,
+      thenRadio.value
+    );
+    // console.log("THEN Answer *************\n" + JSON.stringify(rootCase, undefined, 2));
   }
 
   submit(form: HTMLInputElement) {
     this.service.patchQuestion(this.cumulativeQuestion);
+  }
+
+  // ************** HELPER METHODS ************** //
+
+  getImageURL() {
+    return this.cumulativeQuestion.img;
+  }
+
+  createQuestionMedia() {
+    let media = new Media();
+
+    let questionMedia = new QuestionMedia();
+    questionMedia.setMediaURL(this.getImageURL());
+    questionMedia.setMedia(media);
+
+    return questionMedia;
+  }
+
+  createQuestionText(text: string) {
+    let questionText = new QuestionText();
+    questionText.addText(text);
+
+    return questionText;
+  }
+
+  createQuestion(text: string, answerChoices: string[], correctAnswerChoice: string) {
+    let question = new Question();
+    question.setQuestionText(this.createQuestionText(text));
+    question.addQuestionMedia(this.createQuestionMedia());
+    for (var i = 0; i < answerChoices.length; i++) {
+      question.addAnswerChoices(new AnswerChoice(answerChoices[i]));
+    }
+
+    question.addCorrectAnswerChoice(correctAnswerChoice);
+
+    return question;
+  }
+
+  getQuestionAnswering(text: string, answerChoices: string[], answerChoice: string) {
+
+    let question = this.createQuestion(text, answerChoices, answerChoice);
+    let rootCase = new RootCase();
+    rootCase.addQuestions(question);
+
+    return rootCase;
   }
 
 }
