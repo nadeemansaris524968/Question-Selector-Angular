a9cb9383b12c82cf6d4d6bdccafb5030dea949ad
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-12-13 10:29:41 -0800

    Add new navbar with login and logout buttons.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { AuthService } from './../services/auth.service';
+import { JwtHelper } from 'angular2-jwt';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  welcomeMessage = '';
 
   constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    if (localStorage.getItem('token')){
+      let jwtHelper = new JwtHelper();
+      let token = localStorage.getItem('token');
+      let decoded = jwtHelper.decodeToken(token)
+      this.welcomeMessage = 'Welcome ' + decoded.firstName + decoded.lastName + '!';
+    }
   }
   
 }
